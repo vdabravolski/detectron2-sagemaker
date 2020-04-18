a5f0bed55b2ee311359f28724ba4a4747014ea8a
EC2 Default User <ec2-user@ip-172-16-51-36.us-east-2.compute.internal>
2020-04-18 18:11:05 +0000

    add optional :tag and usage examples

--- build_and_push.sh
+++ build_and_push.sh
@@ -3,10 +3,17 @@
 # This script shows how to build the Docker image and push it to ECR to be ready for use
 # by SageMaker.
 
-# The argument to this script is the image name. This will be used as the image on the local
-# machine and combined with the account and region to form the repository name for ECR.
+# There are 3 arguments in this script:
+#    - image - required, this will be used as the image on the local machine and combined with the account and region to form the repository name for ECR;
+#    - tag - optional, if provided, it will be used as ":tag" of your image; otherwise, ":latest" will be used;
+#    - Dockerfile - optional, if provided, then docker will try to build image from provided dockerfile (e.g. "Dockerfile.serving"); otherwise, default "Dcokerfile" will be used.
+# Usage examples:
+#    1. "./build_and_push.sh d2-sm-coco-serving debug Dockerfile.serving"
+#    2. "./build_and_push.sh d2-sm-coco v2"
+
 image=$1
-dockerfile=$2
+tag=$2
+dockerfile=$3
 
 if [ "$image" == "" ]
 then
@@ -16,8 +23,7 @@ fi
 
 chmod +x container_training/train_balloon.py
 chmod +x container_training/train_coco.py
-
-#rm -r container_training/detectron2 # TODO: delete it eventually
+chmod +x container_serving/predict_coco.py
 
 # Get the account number associated with the current IAM credentials
 account=$(aws sts get-caller-identity --query Account --output text)
@@ -32,8 +38,12 @@ fi
 region=$(aws configure get region)
 region=${region:-us-east-2}
 
-
-fullname="${account}.dkr.ecr.${region}.amazonaws.com/${image}:latest"
+if [ "$tag" == "" ]
+then
+    fullname="${account}.dkr.ecr.${region}.amazonaws.com/${image}:latest"
+else
+    fullname="${account}.dkr.ecr.${region}.amazonaws.com/${image}:${tag}"
+fi
 
 # If the repository doesn't exist in ECR, create it.
 
